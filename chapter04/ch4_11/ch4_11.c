62b01b5fcdb4d0b9e9b049035f43e555790433f7
epqlffltm <kjh55984249@gmail.com>
2026-02-04 06:05:32 +0900

    ch4_11

---
+++ chapter04/ch4_11/ch4_11.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+  FILE *rfp, *wfp;
+  int c = 0;
+
+  if ((rfp = fopen("linux.txt","r"))==NULL)
+  {
+    perror("fopen:linux.txt");
+    exit(1);
+  }
+
+  if((wfp = fopen("linux.out","w")) == NULL);
+  {
+    perror("fopen: linux.out");
+    exit(1);
+  }
+
+  fclose(rfp);
+  fclose(wfp);
+
+  return 0;
+}
