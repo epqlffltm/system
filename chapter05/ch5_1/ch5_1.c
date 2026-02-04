524a87eb7f4d2c8d70a7cd284637b11ccfe1d0aa
epqlffltm <kjh55984249@gmail.com>
2026-02-04 19:48:04 +0900

    ch5_1

---
+++ chapter05/ch5_1/ch5_1.c
@@ -0,0 +1,27 @@
+/*
+2026-02-04
+uname()함수로 운영체제 정보 검색하기
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/utsname.h>
+
+int main(void)
+{
+  struct utsname uts;
+
+  if(uname(&uts) == -1)
+  {
+    perror("uname");
+    exit(1);
+  }
+
+  printf("OS name %s\n", uts.sysname);
+  printf("node name %s\n",uts.nodename);
+  printf("release %s\n",uts.release);
+  printf("version %s\n",uts.version);
+  printf("maxhine %s\n", uts.maxhine);
+
+  return 0;
+}
