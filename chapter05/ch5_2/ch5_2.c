dff9d1bf0ad3435b259dcf56f09620e8d21f0d20
epqlffltm <kjh55984249@gmail.com>
2026-02-05 18:54:09 +0900

    ch5_2

--- chapter05/ch5_2/ch5_2.c
+++ chapter05/ch5_2/ch5_2.c
@@ -1,12 +1,19 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
-  printf("%ld\n",sysconf(_SC_ARG_MAX));
-  printf("%ld\n",sysconf(_SC_CLK_TCK));
-  printf("%ld\n",syscinf(_SC_OPEN_MAX));
-  printf("%ld\n",syscinf(_SC_LOGIN_NAME_MAX));
+    // _SC_ARG_MAX: exec 함수 호출 시 인자와 환경 변수의 최대 합계 바이트 수
+    printf("ARG_MAX: %ld\n", sysconf(_SC_ARG_MAX));
 
-  return 0;
+    // _SC_CLK_TCK: 초당 클록 틱 수 (times 함수 등에서 사용)
+    printf("CLK_TCK: %ld\n", sysconf(_SC_CLK_TCK));
+
+    // _SC_OPEN_MAX: 프로세스가 동시에 열 수 있는 최대 파일 수
+    printf("OPEN_MAX: %ld\n", sysconf(_SC_OPEN_MAX));
+
+    // _SC_LOGIN_NAME_MAX: 로그인 이름의 최대 길이 (널 문 포함)
+    printf("LOGIN_NAME_MAX: %ld\n", sysconf(_SC_LOGIN_NAME_MAX));
+
+    return 0;
 }
