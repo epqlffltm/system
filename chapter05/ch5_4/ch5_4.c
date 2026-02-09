730a4e4493327c82f3f02e5157d0508dd5e84287
epqlffltm <kjh55984249@gmail.com>
2026-02-09 14:29:34 +0900

    오류 수정

--- chapter05/ch5_4/ch5_4.c
+++ chapter05/ch5_4/ch5_4.c
@@ -4,13 +4,12 @@ getuid(), feteuid()함수로 UID, EUID 검색하기
 */
 
 #include<stdio.h>
-#include<stdlib.h>
 #include<unistd.h>
-#include<sys/type.h>
+#include<sys/types.h>
 
 int main(void)
 {
-  uid_t uid = getuid(), edid = geteuid();
+  uid_t uid = getuid(), euid = geteuid();
   char *name = getlogin();
 
   printf("name = %s, UID = %d, EUID = %d\n",name,(int)uid,(int)euid);
